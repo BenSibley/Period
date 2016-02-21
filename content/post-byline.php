72edadac9c950f6b9394636fecfe34d9ee0f3347
BenSibley <ben@competethemes.com>
2016-02-21 10:13:58 -0500

    archive styling

--- content/post-byline.php
+++ content/post-byline.php
@@ -1,16 +1,7 @@
 <div class="post-byline">
-    <span class="post-date">
-		<?php
-		$date = date_i18n( get_option( 'date_format' ), strtotime( get_the_date( 'r' ) ) );
-		printf( __( 'Published %s', 'period' ), $date );
-		?>
-	</span>
 	<?php
-	$author = get_theme_mod( 'author_byline' );
-	if ( $author == 'yes' ) { ?>
-		<span class="post-author">
-			<span><?php _e( 'By', 'period' ); ?></span>
-			<?php the_author(); ?>
-		</span>
-	<?php } ?>
+	$author = "<a class='author' href='" . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . "'>" . get_the_author() . "</a>";
+	$date   = "<a class='date' href='" . esc_url( get_month_link( get_the_date( 'Y' ), get_the_date( 'n' ) ) ) . "'>" . date_i18n( get_option( 'date_format' ), strtotime( get_the_date( 'r' ) ) ) . "</a>";
+	printf( _x( 'Published %1$s by %2$s', 'This blog post was published on some date by some author', 'period' ), $date, $author );
+	?>
 </div>
