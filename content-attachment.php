db6e0bae0c0fcbaac4c8aacb115d072ea7a457fe
BenSibley <ben@competethemes.com>
2016-04-08 13:54:57 -0400

    improved attachment page styling, and now using full size images, captions, and descriptions

--- content-attachment.php
+++ content-attachment.php
@@ -1,12 +1,26 @@
 <div <?php post_class(); ?>>
 	<?php do_action( 'attachment_before' ); ?>
 	<article>
-		<div class='post-header'>
-			<h1 class='post-title'><?php the_title(); ?></h1>
-		</div>
-		<div class="post-content">
-			<?php the_content(); ?>
-			<?php get_template_part( 'content/post-nav-attachment' ); ?>
+		<div class="post-container">
+			<div class='post-header'>
+				<h1 class='post-title'><?php the_title(); ?></h1>
+			</div>
+			<div class="post-content">
+				<?php
+				$image = wp_get_attachment_image($post->ID, 'full');
+				$image_meta = wp_prepare_attachment_for_js($post->ID);
+				?>
+				<div class="attachment-container">
+					<?php echo $image; ?>
+					<span class="attachment-caption">
+						<?php echo $image_meta['caption']; ?>
+					</span>
+				</div>
+				<?php echo wpautop( $image_meta['description'] ); ?>
+				<div class="post-meta">
+					<?php get_template_part( 'content/post-nav-attachment' ); ?>
+				</div>
+			</div>
 		</div>
 	</article>
 	<?php do_action( 'attachment_after' ); ?>
--- content/post-nav-attachment.php
+++ content/post-nav-attachment.php
@@ -1,8 +1,8 @@
 <nav class="further-reading">
 	<div class="previous">
-		<span><?php previous_image_link( false, __( 'Previous Image', 'period' ) ); ?></span>
+		<?php previous_image_link( false, __( 'Previous Image', 'period' ) ); ?>
 	</div>
 	<div class="next">
-		<span><?php next_image_link( false, __( 'Next Image', 'period' ) ); ?></span>
+		<?php next_image_link( false, __( 'Next Image', 'period' ) ); ?>
 	</div>
 </nav>
