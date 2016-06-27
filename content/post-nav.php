a85fcd39a7979836aa783eddb00e205c23626d1b
BenSibley <ben@competethemes.com>
2016-06-27 08:40:36 -0400

    simplified post-nav with use of previous/next_post_link

--- content/post-nav.php
+++ content/post-nav.php
@@ -4,56 +4,46 @@ global $post;
 
 // gets the previous post if it exists
 $previous_post = get_adjacent_post(false,'',true);
+$previous_text = __('Previous Post', 'period');
 
-// if there is a previous post
-if( $previous_post ) {
-	// text above the link
-	$previous_text = __('Previous Post', 'period');
-	// if there is a title use it, else call it "The Previous Post"
-	$previous_title = get_the_title( $previous_post ) ? get_the_title( $previous_post ) : __("The Previous Post", 'period');
-	// get the post link
-	$previous_link = get_permalink( $previous_post );
-}
-// if there isn't a previous post
-else {
-	// text above the link
+if ( $previous_post == '' ) {
 	$previous_text = __('No Older Posts', 'period');
-	// set the title to return to the blog
 	$previous_title = __('Return to Blog', 'period');
-	// link to blog
-	$previous_link = home_url();
+	$previous_url = home_url();
+	$previous_link = '<a href="' . esc_url( $previous_url ) . '">' . esc_html( $previous_title ) . '</a>';
 }
 
 // gets the next post if it exists
 $next_post = get_adjacent_post(false,'',false);
+$next_text = __('Next Post', 'period');
 
-// if there is a next post
-if( $next_post ) {
-	// text above the link
-	$next_text = __('Next Post', 'period');
-	// if there is a title use it, else call it "The next Post"
-	$next_title = get_the_title( $next_post ) ? get_the_title( $next_post ) : __("The Next Post", 'period');
-	// get the post link
-	$next_link = get_permalink( $next_post );
-}
-// if there isn't a next post
-else {
-	// text above the link
+if( $next_post == '' ) {
 	$next_text = __('No Newer Posts', 'period');
-	// set the title to return to the blog
 	$next_title = __('Return to Blog', 'period');
-	// link to blog
-	$next_link = home_url();
+	$next_url = home_url();
+	$next_link = '<a href="' . esc_url( $next_url ) . '">' . esc_html( $next_title ) . '</a>';
 }
 
 ?>
 <nav class="further-reading">
 	<div class="previous">
 		<span><?php echo esc_html( $previous_text ); ?></span>
-		<a href="<?php echo esc_url( $previous_link ); ?>"><?php echo esc_html( $previous_title ); ?></a>
+		<?php
+		if ( $previous_post == '' ) {
+			echo $previous_link;
+		} else {
+			previous_post_link( '%link' );
+		}
+		?>
 	</div>
 	<div class="next">
 		<span><?php echo esc_html( $next_text ); ?></span>
-		<a href="<?php echo esc_url( $next_link ); ?>"><?php echo esc_html( $next_title ); ?></a>
+		<?php
+		if ( $next_post == '' ) {
+			echo $next_link;
+		} else {
+			next_post_link( '%link' );
+		}
+		?>
 	</div>
 </nav>
