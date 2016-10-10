d5d19cc90d32bce6697d37fb7051561a7ee25e6c
BenSibley <ben@competethemes.com>
2016-10-10 07:59:04 -0400

    content width variable wrapped in function

--- functions.php
+++ functions.php
@@ -1,8 +1,13 @@
 <?php
 
-if ( ! isset( $content_width ) ) {
-	$content_width = 891;
+if ( ! function_exists( ( 'ct_period_set_content_width' ) ) ) {
+	function ct_period_set_content_width() {
+		if ( ! isset( $content_width ) ) {
+			$content_width = 891;
+		}
+	}
 }
+add_action( 'after_setup_theme', 'ct_period_set_content_width', 0 );
 
 if ( ! function_exists( ( 'ct_period_theme_setup' ) ) ) {
 	function ct_period_theme_setup() {
