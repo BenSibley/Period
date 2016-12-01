defee04310f10986f9b4d684921616dae300f2b8
BenSibley <ben@competethemes.com>
2016-12-01 13:23:39 -0500

    updated dashboard stuff

--- theme-options.php
+++ theme-options.php
@@ -18,17 +18,9 @@ function ct_period_options_content() {
 	<div id="period-dashboard-wrap" class="wrap">
 		<h2><?php _e( 'Period Dashboard', 'period' ); ?></h2>
 		<?php do_action( 'theme_options_before' ); ?>
-		<div class="content content-customization">
-			<h3><?php _e( 'Customization', 'period' ); ?></h3>
-			<p><?php _e( 'Click the "Customize" link in your menu, or use the button below to get started customizing Period', 'period' ); ?>.</p>
-			<p>
-				<a class="button-primary"
-				   href="<?php echo esc_url( $customizer_url ); ?>"><?php _e( 'Use Customizer', 'period' ) ?></a>
-			</p>
-		</div>
 		<div class="content content-support">
-			<h3><?php _e( 'Support', 'period' ); ?></h3>
-			<p><?php _e( "You can find the knowledgebase, changelog, support forum, and more in the Period Support Center", "period" ); ?>.</p>
+			<h3><?php _e( 'Get Started', 'period' ); ?></h3>
+			<p><?php _e( "Not sure where to start? The Period Support Center is filled with tutorials that will take you step-by-step through every feature in Period.", "period" ); ?></p>
 			<p>
 				<a target="_blank" class="button-primary"
 				   href="https://www.competethemes.com/documentation/period-support-center/"><?php _e( 'Visit Support Center', 'period' ); ?></a>
@@ -40,14 +32,6 @@ function ct_period_options_content() {
 			<p>
 				<a target="_blank" class="button-primary"
 				   href="https://www.competethemes.com/period-pro/"><?php _e( 'See Full Feature List', 'period' ); ?></a>
-			</p>
-		</div>
-		<div class="content content-resources">
-			<h3><?php _e( 'WordPress Resources', 'period' ); ?></h3>
-			<p><?php _e( 'Save time and money searching for WordPress products by following our recommendations', 'period' ); ?>.</p>
-			<p>
-				<a target="_blank" class="button-primary"
-				   href="https://www.competethemes.com/wordpress-resources/"><?php _e( 'View Resources', 'period' ); ?></a>
 			</p>
 		</div>
 		<div class="content content-review">
