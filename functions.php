b6ded3c7d9c1bffeb807cde437d7238ec21f441c
BenSibley <ben@competethemes.com>
2016-12-01 13:09:50 -0500

    added redirect to dashboard page

--- functions.php
+++ functions.php
@@ -608,3 +608,16 @@ if ( ! function_exists( ( 'ct_period_allow_skype_protocol' ) ) ) {
 	}
 }
 add_filter( 'kses_allowed_protocols' , 'ct_period_allow_skype_protocol' );
+
+// trigger theme switch on link click and send to Appearance menu
+function ct_period_welcome_redirect() {
+
+	$welcome_url = add_query_arg(
+		array(
+			'page' => 'period-options'
+		),
+		admin_url( 'themes.php' )
+	);
+	wp_redirect( esc_url( $welcome_url ) );
+}
+add_action( 'after_switch_theme', 'ct_period_welcome_redirect' );
