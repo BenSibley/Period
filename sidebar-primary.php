4bcffe5d4e69087063f8c497fc0ae58cf75e6b0e
BenSibley <ben@competethemes.com>
2017-08-10 12:27:51 -0400

    added full-width template and updaated tags

--- sidebar-primary.php
+++ sidebar-primary.php
@@ -1,4 +1,8 @@
-<?php if ( is_active_sidebar( 'primary' ) ) : ?>
+<?php
+if ( is_page_template( 'templates/full-width.php' ) ) {
+    return;
+}
+if ( is_active_sidebar( 'primary' ) ) : ?>
     <aside class="sidebar sidebar-primary" id="sidebar-primary" role="complementary">
         <h1 class="screen-reader-text"><?php esc_html_e('Sidebar', 'period'); ?></h1>
         <?php dynamic_sidebar( 'primary' ); ?>
