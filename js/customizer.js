5a33204fe5e2e0ae3cb33f6540801458ce3b26d5
BenSibley <ben@competethemes.com>
2017-12-07 10:32:30 -0500

    fix for layouts

--- js/customizer.js
+++ js/customizer.js
@@ -16,11 +16,11 @@ jQuery(document).ready(function($){
 
         // add the appropriate image to each label
         layoutInputs.each( function() {
-            $(this).parent().css('background-image', 'url("../wp-content/themes/period/assets/images/' + $(this).val() + '.png")');
+            $(this).next().css('background-image', 'url("../wp-content/themes/period/assets/images/' + $(this).val() + '.png")');
 
             // add initial 'selected' class
             if ($(this).prop('checked')) {
-                $(this).parent().addClass('selected');
+                $(this).next().addClass('selected');
             }
         });
 
@@ -34,11 +34,11 @@ jQuery(document).ready(function($){
     function addSelectedLayoutClass(inputs, target) {
 
         // remove 'selected' class from all labels
-        inputs.parent().removeClass('selected');
+        inputs.next().removeClass('selected');
 
         // apply 'selected' class to :checked input
         if (target.prop('checked')) {
-            target.parent().addClass('selected');
+            target.next().addClass('selected');
         }
     }
 });
