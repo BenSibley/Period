4e028ab1c075a0255eaaa3b59d46d8890b99fd2f
BenSibley <ben@competethemes.com>
2018-12-04 17:58:26 -0500

    added Goodreads social icon

--- functions.php
+++ functions.php
@@ -362,6 +362,7 @@ if ( ! function_exists( 'ct_period_social_array' ) ) {
 			'flickr'        => 'period_flickr_profile',
 			'foursquare'    => 'period_foursquare_profile',
 			'github'        => 'period_github_profile',
+			'goodreads'			=> 'period_goodreads_profile',
 			'google-plus'   => 'period_googleplus_profile',
 			'google-wallet' => 'period_google_wallet_profile',
 			'hacker-news'   => 'period_hacker-news_profile',
