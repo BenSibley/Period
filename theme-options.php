bca25370a8e8b82eb4798d68d5cbc550783e52ed
BenSibley <ben@competethemes.com>
2019-11-13 15:19:51 -0500

    fixed PHP notice

--- theme-options.php
+++ theme-options.php
@@ -1,7 +1,13 @@
 <?php
 
 function ct_period_register_theme_page() {
-	add_theme_page( sprintf( esc_html__( '%s Dashboard', 'period' ), wp_get_theme() ), sprintf( esc_html__( '%s Dashboard', 'period' ), wp_get_theme() ), 'edit_theme_options', 'period-options', 'ct_period_options_content', 'ct_period_options_content' );
+	add_theme_page( 
+		sprintf( esc_html__( '%s Dashboard', 'period' ), wp_get_theme() ), 
+		sprintf( esc_html__( '%s Dashboard', 'period' ), wp_get_theme() ), 
+		'edit_theme_options', 
+		'period-options', 
+		'ct_period_options_content'
+	);
 }
 add_action( 'admin_menu', 'ct_period_register_theme_page' );
 
