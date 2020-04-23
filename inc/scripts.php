c5b293cbd985fc735c48f60bc17d88e8e8271456
BenSibley <ben@competethemes.com>
2020-04-23 10:39:25 -0400

    added font swap to GF

--- inc/scripts.php
+++ inc/scripts.php
@@ -4,8 +4,9 @@
 function ct_period_load_scripts_styles() {
 
 	$font_args = array(
-		'family' => urlencode( 'Roboto:300,300italic,400,700' ),
-		'subset' => urlencode( 'latin,latin-ext' )
+		'family'  => urlencode( 'Roboto:300,300italic,400,700' ),
+		'subset'  => urlencode( 'latin,latin-ext' ),
+		'display' => 'swap'
 	);
 	$fonts_url = add_query_arg( $font_args, '//fonts.googleapis.com/css' );
 	
