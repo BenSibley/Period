8f39ab18b2e81ebd83f8868fe5bc9b3a4014e89e
BenSibley <ben@competethemes.com>
2020-04-23 10:38:45 -0400

    updated Customizer ad

--- inc/customizer.php
+++ inc/customizer.php
@@ -564,7 +564,7 @@ function ct_period_sanitize_phone( $input ) {
 function ct_period_customize_preview_js() {
 	if ( !function_exists( 'ct_period_pro_init' ) ) {
 		$url = 'https://www.competethemes.com/period-pro/?utm_source=wp-dashboard&utm_medium=Customizer&utm_campaign=Period%20Pro%20-%20Customizer';
-		$content = "<script>jQuery('#customize-info').prepend('<div class=\"upgrades-ad\"><a href=\"". $url ."\" target=\"_blank\">Get New Layouts with Period Pro <span>&rarr;</span></a></div>')</script>";
+		$content = "<script>jQuery('#customize-info').prepend('<div class=\"upgrades-ad\"><a href=\"". $url ."\" target=\"_blank\">Customize Colors with Period Pro <span>&rarr;</span></a></div>')</script>";
 		echo apply_filters('ct_period_customizer_ad', $content);
 	}
 }
