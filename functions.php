e1ea8adc6088e7e087d73d3ac2eedee289730e91
BenSibley <ben@competethemes.com>
2020-08-25 10:45:02 -0400

    no longer adding 'more link' to RSS feeds

--- functions.php
+++ functions.php
@@ -227,6 +227,10 @@ add_action( 'comment_form_defaults', 'ct_period_remove_comments_notes_after' );
 
 if ( ! function_exists( 'ct_period_filter_read_more_link' ) ) {
 	function ct_period_filter_read_more_link( $custom = false ) {
+
+		if ( is_feed() ) {
+			return;
+		}
 		global $post;
 		$ismore             = strpos( $post->post_content, '<!--more-->' );
 		$read_more_text     = get_theme_mod( 'read_more_text' );
